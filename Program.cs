54b276c83bed99bc63120aeec63f7ca01b727c8a
MikhaZyu <MikhaZyu@gmail.com>
2021-05-30 20:47:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.CSharp.HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
